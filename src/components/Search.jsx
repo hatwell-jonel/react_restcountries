de87cf898dd950e59f0b372dc4cef14d5f8cb549
Jonel Hatwell <jonel.hatwell@gmail.com>
2023-10-19 17:51:56 +0800

    temporary search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {BiSearchAlt2} from "react-icons/bi"
 
-function Search() {
+function Search({search, setSearch}) {
   return (
     <div className='search'>
-      <input type="text" placeholder="Search..." />
+      <input type="text" placeholder="Search..." onChange={(e) => setSearch(`name/${e.target.value}`)}/>
       <BiSearchAlt2 />
     </div>
   )
--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -6,34 +6,45 @@ import Card from '../components/Card'
 
 function Countries() {
     const [countries, setCountries] = useState([]);
-    const API_RESTCOUNTRIES = "https://restcountries.com/v3.1/all";
-  
-    useEffect(() => {
-      const fetchCountries = async () => {
+    const [api, setApi] = useState('https://restcountries.com/v3.1');
+    const [endpoint, setEndpoint] = useState("all");
+    const [search, setSearch] = useState("");
+
+
+    
+    const fetchCountries = async () => {
         try {
-          const response = await fetch(API_RESTCOUNTRIES);
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          const data = await response.json();
+            if(search == "name/"){
+                setEndpoint("all"); 
+            }else{
+                setEndpoint(search) 
+            }
+
+            const response = await fetch(`${api}/${endpoint}`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
   
-          // Sort the countries array alphabetically by name
-          data.sort((a, b) => a.name.common.localeCompare(b.name.common));
-  
-          setCountries(data);
+            // Sort the countries array alphabetically by name
+            data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+    
+            setCountries(data);
         } catch (error) {
           console.error('Error fetching data:', error);
         }
       }
+      
+    useEffect(() => {
       fetchCountries();
-    }, [])
+    }, [search])
 
 
   return (
      <>
       <Header />
       <div className='filter'>
-        <Search />
+        <Search search={search} setSearch={setSearch} />
         <Filter />
       </div>
       <section className="countries">
@@ -46,3 +57,7 @@ function Countries() {
 }
 
 export default Countries
+
+// https://restcountries.com/v3.1/all
+// https://restcountries.com/v3.1/name/{name}
+// https://restcountries.com/v3.1/region/{region}
